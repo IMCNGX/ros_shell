2b3e5d2142faed26ad79caea8d388ce385562da0
IMCNGX <42267896+IMCNGX@users.noreply.github.com>
2018-10-16 22:22:27 +0800

    Add files via upload

---
+++ Creating_Package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please enter your workspace path:"
+read path
+echo "Please enter a name for a new package:"
+read name
+cd /$path/src
+source /opt/ros/<distro>/setup.bash
+source devel/setup.bash
+catkin_create_pkg $name std_msgs rospy roscpp
+echo "I have make a package named $name for you!"
