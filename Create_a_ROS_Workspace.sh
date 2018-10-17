5ec828088eb55312533c71768f874b03287c28f5
IMCNGX <42267896+IMCNGX@users.noreply.github.com>
2018-10-17 21:27:48 +0800

    Update Create_a_ROS_Workspace.sh

--- Create_a_ROS_Workspace.sh
+++ Create_a_ROS_Workspace.sh
@@ -3,8 +3,8 @@ echo "Please input your path:"
 read path
 echo "Please enter a name for a new workspace:"
 read name
-mkdir -p /$path/$name/src
-cd /$path/$name/
+mkdir -p $path/$name/src
+cd $path/$name
 source /opt/ros/<distro>/setup.bash
 source devel/setup.bash
 catkin_make
