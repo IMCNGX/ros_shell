fc22072780b29e84161387fcbcf270b2a8f3e59b
IMCNGX <42267896+IMCNGX@users.noreply.github.com>
2018-10-17 21:31:37 +0800

    Update Creating_Package.sh

--- Creating_Package.sh
+++ Creating_Package.sh
@@ -3,8 +3,9 @@ echo "Please enter your workspace path:"
 read path
 echo "Please enter a name for a new package:"
 read name
-cd $path/src
+cd $path
 source /opt/ros/kinetic/setup.bash
 source devel/setup.bash
+cd $path/src
 catkin_create_pkg $name std_msgs rospy roscpp
 echo "I have make a package named $name for you!"
